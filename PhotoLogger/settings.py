3c4ab4f39e5fa1e9ddf6c141b07891d6c80cbab2
Shigeo Kitamura <kitamura@amateras.wsd.kutc.kansai-u.ac.jp>
2018-11-08 22:21:41 +0900

    changed for deploy

--- PhotoLogger/settings.py
+++ PhotoLogger/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/photologger/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/photologger/media/'
