0d059c53b4b8e52c31461fc10ac8cc19cb1483e5
Shigeo Kitamura <shigeo9462@gmail.com>
2018-11-10 23:18:32 +0900

    get device heading

--- photomap/static/photomap/location.js
+++ photomap/static/photomap/location.js
@@ -28,11 +28,42 @@ function onLocationError(e) {
     alert(e.message);
 }
 
-function onRotated(angle) {
-    console.log(angle);
-    dir.innerText = `方位: ${angle}`;
-}
-
 map.on('locationfound', onLocationFound);
 map.on('locationerror', onLocationError);
 map.on('compass:rotated', onRotated);
+
+function compassHeading(alpha, beta, gamma) {
+    const degtorad = Math.PI / 180; // Degree-to-Radian conversion
+  
+    const _x = beta ? beta * degtorad : 0; // beta value
+    const _y = gamma ? gamma * degtorad : 0; // gamma value
+    const _z = alpha ? alpha * degtorad : 0; // alpha value
+  
+    const cX = Math.cos(_x);
+    const cY = Math.cos(_y);
+    const cZ = Math.cos(_z);
+    const sX = Math.sin(_x);
+    const sY = Math.sin(_y);
+    const sZ = Math.sin(_z);
+  
+    // Calculate Vx and Vy components
+    const Vx = -cZ * sY - sZ * sX * cY;
+    const Vy = -sZ * sY + cZ * sX * cY;
+  
+    // Calculate compass heading
+    let compassHeading = Math.atan(Vx / Vy);
+  
+    // Convert compass heading to use whole unit circle
+    if (Vy < 0) {
+      compassHeading += Math.PI;
+    } else if (Vx < 0) {
+      compassHeading += 2 * Math.PI;
+    }
+  
+    return compassHeading * ( 180 / Math.PI ); // Compass Heading (in degrees)
+}
+
+window.addEventListener('deviceorientation', event => {
+    const heading = compassHeading(event.alpha, event.beta, event.gamma);
+    dir.innerText = `方位: ${heading}`;
+});
