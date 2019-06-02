cc4e6a46cf29dfab2dea5de25f0dccb75ea336ee
Shigeo Kitamura <shigeo9462@gmail.com>
2019-06-02 17:09:55 +0900

    changed root path

--- PhotoLogger/settings.py
+++ PhotoLogger/settings.py
@@ -115,7 +115,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-ROOT_URL = '/photologger/'
+ROOT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
